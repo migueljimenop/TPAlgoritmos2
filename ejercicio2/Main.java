4f6ba74005637017a0cd297f5b9cec779ffcd83f
Miguel Jimeno <migueljimeno.p@gmail.com>
2017-05-14 18:10:03 -0300

    Modificacion de backlog

--- ejercicio2/Main.java
+++ ejercicio2/Main.java
@@ -1,16 +1,41 @@
+import java.io.*; 
 public class Main{
-  public static void main(String[] args) {
-  	//lectura del archivo
-  	//creacion del heap
-	Heap heap= new Heap(20); 
-	//lectura de las lineas del archivo y creacion de las cuadruplas
-	//cargar el heap
-	//generacion de los Sprints
-	//mostrar los Sprints(ciclo)
-	//creacion de nuevo archivo
+	public static void main(String[] args) throws Exception {
+		// Lectura de archivo para obtener la cantidad de tareas.
+		FileReader fr = new FileReader("backlog.txt"); 
+		BufferedReader br = new BufferedReader(fr); 
+		String s;
+		int cont = 0; 
+		while((s = br.readLine()) != null) { 
+	  		cont++;
+		}
+		// Creacion del heap con la capacidad.
+		Heap heap = new Heap(cont); 
 
+		//System.out.println(cont);
+		//System.out.println(heap.capacidad);
 
-  }
+		//lectura de las lineas del archivo y creacion de las cuadruplas
+	 	fr = new FileReader("backlog.txt");
+ 		br = new BufferedReader(fr); 
+		String[] array;
+		while((s = br.readLine()) != null) { 
+			//System.out.println(s); 
+			array = divideString(s);
+			int valor = (int) array[1];
+			int costo = (int) array[2];
+			Cuadrupla cp = new Cuadrupla(array[0], valor, costo);
+			// Carga del heap.
+			heap.insertar(cp);		
+		}	
 
+		System.out.println(heap.longitud());
 
+		//generacion de los Sprints
+		//mostrar los Sprints(ciclo)
+
+		fr.close();
+
+		//creacion de nuevo archivo
+		}
 }
