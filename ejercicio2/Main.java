674aa127b900c69c72312da9f9dd90c81332c1a1
sjcuello <santy.sc@hotmail.com>
2017-05-13 22:08:20 -0300

    Añado los archivos de backlog y main

---
+++ ejercicio2/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+  public static void main(String[] args) {
+  	//lectura del archivo
+  	//creacion del heap
+	Heap heap= new Heap(20); 
+	//lectura de las lineas del archivo y creacion de las cuadruplas
+	//cargar el heap
+	//generacion de los Sprints
+	//mostrar los Sprints(ciclo)
+	//creacion de nuevo archivo
+
+
+  }
+
+
+}
